6d75d024ea5553210267fc085ed3dd0e9bf057d8
fangting9 <974931145@qq.com>
2022-06-19 19:12:50 +0800

    fix

--- huipei-front/src/components/customer/SignUp.vue
+++ huipei-front/src/components/customer/SignUp.vue
@@ -5,7 +5,8 @@
                 <el-input v-model="signUpInfo.name" placeholder="请输入姓名"></el-input>
             </el-form-item>
             <el-form-item label="电话">
-                <el-input v-model="signUpInfo.phone" placeholder="请输入手机号"></el-input>
+                <el-input v-model="signUpInfo.phone" placeholder="请输入手机号" oninput="value=value.replace(/[^\d]/g,'')"
+                          maxlength="11" @blur="telTest"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="signUpSave()" style="width: 100%;height:40px;color: white" round>立即报名</el-button>
@@ -45,6 +46,12 @@
                 commonData.getSid(req);
                 this.$emit('change', false)
                 ElMessage.success("报名成功")
+            },
+            telTest () {
+                const reg = /^1([38]\d|5[0-35-9]|7[3678])\d{8}$/;
+                if (this.chatPhone=== '' || this.chatPhone.length <= 10 || !reg.test(this.chatPhone)) {
+                    ElMessage.error("手机号码错误");
+                }
             },
         }
     }
